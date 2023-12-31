55fd229d4360a4e1553b7b3ea1d6065dfb9bff36
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2023-12-31 15:28:07 +0200

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,97 @@
+public class Circle {
+    private double radius;
+    private Point center;
+    public Circle(int x, int y, double radius)
+    {
+        this.radius = radius;
+        this.center = new Point(x, y);
+    }
+
+    public Circle(Circle other)
+    {
+        this.center = new Point(other.center);
+        this.radius = other.radius;
+    }
+
+    public Circle(Point center, double radius)
+    {
+        this.radius = radius;
+        this.center = new Point(center);
+    }
+
+
+    public double getRadius(double radius)
+    {
+        return radius;
+    }
+
+    public void setRadius(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public Point getCenter()
+    {
+        return this.center;
+    }
+
+
+    public void setCenter(Point center)
+    {
+        this.center = getCenter();
+    }
+
+    public double perimeter()
+    {
+        return 2 * Math.PI * this.radius;
+    }
+
+    public double area()
+    {
+        return Math.PI * (this.radius * this.radius);
+    }
+
+    public void move(int deltaX, int deltaY)
+    {
+        Point new_point = new Point(deltaX, deltaY);
+        setCenter(new_point);
+    }
+
+    public boolean equals(Circle other)
+    {
+        if(this.radius == other.radius)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isLarger(Circle other)
+    {
+        if (this.area() > other.area())
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public boolean isInCircle(Point p)
+    {
+        if (p.distance(this.center) < this.radius)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public String toString()
+    {
+        return("The center of the circle = (" + this.center + "and the radius = " + this.radius);
+    }
+
+}
+
