7e8ad5b5d958df6b740c627f245e475e78f69db8
Gezraf <59087430+GezrafYT@users.noreply.github.com>
2024-01-15 23:48:39 +0200

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -20,9 +20,9 @@ public class Circle {
     }
 
 
-    public double getRadius(double radius)
+    public double getRadius()
     {
-        return radius;
+        return this.radius;
     }
 
     public void setRadius(double radius)
